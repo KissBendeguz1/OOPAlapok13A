0c541faae151927409293a0f6a07907f8400b257
KissBendeguz1 <kissb@kkszki.hu>
2026-09-07 11:40:36 +0200

    initial commit, személy class + more already been created

---
+++ Szemely.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Alapok
+{
+    public class Szemely
+    {
+        string Nev;
+        private int Eletkor;
+
+        public Szemely(string nev, int eletkor)
+        {
+            Nev = nev;
+            Eletkor = eletkor;
+        }
+
+
+    }
+}
